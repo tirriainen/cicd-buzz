062e64f0487ce5c5e7f92020fac9bbeaef55a528
Tiia Rantanen <tiia.rantanen@zeelandfamily.fi>
2018-05-27 14:21:06 +0300

    removed TAG

--- .travis/deploy_dockerhub.sh
+++ .travis/deploy_dockerhub.sh
@@ -1,10 +1,5 @@
 #!/bin/sh
 docker login --username $DOCKER_USER --password $DOCKER_PASS
-if [ "$TRAVIS_BRANCH" = "master" ]; then
-    TAG="latest"
-else
-    TAG="$TRAVIS_BRANCH"
-fi
-DOCKER_PATH=$DOCKER_USER/$DOCKERHUB_REPO:$TAG
-docker build -f Dockerfile -t $DOCKER_PATH:$TAG .
+DOCKER_PATH=$DOCKER_USER/$DOCKERHUB_REPO:latest
+docker build -f Dockerfile -t $DOCKER_PATH:latest .
 docker push $DOCKER_PATH
