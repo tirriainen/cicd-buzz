32256b0ed12bcf975e119b68f0523ce96ca47d7c
Tiia Rantanen <tiia.rantanen@zeelandfamily.fi>
2018-05-27 14:26:55 +0300

    Removed TAG from docker tag as according to docs

--- .travis/deploy_dockerhub.sh
+++ .travis/deploy_dockerhub.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 docker login --username $DOCKER_USER --password $DOCKER_PASS
-DOCKER_PATH=$DOCKER_USER/$DOCKERHUB_REPO:latest
-docker build -f Dockerfile -t $DOCKER_PATH:latest .
+if [ "$TRAVIS_BRANCH" = "master" ]; then
+    TAG="latest"
+else
+    TAG="$TRAVIS_BRANCH"
+fi
+DOCKER_PATH=$DOCKER_USER/$DOCKERHUB_REPO:$TAG
+docker build --file Dockerfile --tag $DOCKER_PATH .
 docker push $DOCKER_PATH
