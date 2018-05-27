862ff0bb663d5e206c7b7f6082d9b55312089538
Tiia Rantanen <tiia.rantanen@zeelandfamily.fi>
2018-05-27 14:06:48 +0300

    Moved DOCKER_PATH after TAG

--- .travis/deploy_dockerhub.sh
+++ .travis/deploy_dockerhub.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
-DOCKER_PATH=$DOCKER_USER/$DOCKERHUB_REPO:$TAG
 docker login -username $DOCKER_USER -password $DOCKER_PASS
 if [ "$TRAVIS_BRANCH" = "master" ]; then
     TAG="latest"
 else
     TAG="$TRAVIS_BRANCH"
 fi
+DOCKER_PATH=$DOCKER_USER/$DOCKERHUB_REPO:$TAG
 docker build -f Dockerfile -t $DOCKER_PATH:$TAG .
 docker push $DOCKER_PATH
